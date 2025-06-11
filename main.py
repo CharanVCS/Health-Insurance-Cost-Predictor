e6875ef37860eab45f6e77af18133d9cff0e5bb9
Charan_VCS <119949807+CharanVCS@users.noreply.github.com>
2025-06-12 00:05:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from prediction_helper import predict
 
 
 # Define the page layout
-st.title('Health Insurance Cost Predictor')
+st.title('Health Insurance Premium Predictor')
 
 categorical_options = {
     'Gender': ['Male', 'Female'],
